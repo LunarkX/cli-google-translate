2b3d68d431eebcfcde50d6321a4bf3fce6f62716
CharonTheV <bikathimartin@outlook.com>
2022-04-22 15:43:48 +0300

    Added first commit

---
+++ src/main/java/npc/martin/cligoogletrans/Translate.java
@@ -0,0 +1,11 @@
+package npc.martin.cligoogletrans;
+
+/**
+ * Hello world!
+ *
+ */
+public class Translate {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
