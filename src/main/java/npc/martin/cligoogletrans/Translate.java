2fadc8c31911063dcd74ac777df44c7f1c89329e
CharonTheV <bikathimartin@outlook.com>
2022-04-26 00:39:46 +0300

    Added logic to handle all command options and errors.

--- src/main/java/npc/martin/cligoogletrans/Translate.java
+++ src/main/java/npc/martin/cligoogletrans/Translate.java
@@ -15,13 +15,14 @@ public class Translate implements Runnable {
     String phrase;
     
     //option-parameter- target language
+    //cannot be used with the '-lcs' flag
     @Option(names = { "-lc", "--language-code" }, paramLabel = "LANGUAGE CODE", 
             description = "Language being translated to.")
     String langCode;
     
     //option- activate interractive mode
     @Option(names = { "-i", "--interractive" }, description = "Run this utility in interractive mode.")
-    private Boolean interractive;
+    protected Boolean interractive = false;
     
     //option- allow for logging of current translation
     @Option(names = { "-l", "--log" }, description = "Allow for logging of a translation")
@@ -34,16 +35,32 @@ public class Translate implements Runnable {
     ArrayList<String> langCodes = new ArrayList(5);
     
     @Override
-    public void run() {
-        //always populate the hashmap on the go first before executing rest of code
-        TranslationProperties.createPairs();
-        
-        if(this.interractive == true) {
-            new InterractiveHandler().takeInput();
-        }
+    public void run() {       
+        //code to handle option errors
+        //1. When the -p flag is not invoked. It must always be used
+        if(phrase == null) {
+            System.out.println("Error! Must provide a phrase or word to translate. Use -h for Help.");    
+        //2. When the '-lc' and '-lcs' flags are used together
+        } else if(langCode != null && langCodes.isEmpty() == false) {
+            System.out.println("Fatal! -lc and -lcs cannot be used together. Use -h for Help.");
+        //if both of these tests pass, then the code can start running
+        } else {
+            //populate the hashmap on the go first before executing rest of code
+            TranslationPrerequisites.createPairs();
+
+            if(this.interractive == true) {
+                new InterractiveHandler().takeInput();
+            } else if(langCode != null) {
+                //do call the makeRequest() method from the class that Handles Single Requests(target)
+                new HandleSingleTarget().makeRequest(phrase, langCode);
+            } else if(langCodes.isEmpty() == false) {
+                //do call the makeMultipleRequests() method from the class that Handles Multiple Requests(targets)
+                new HandleMultiTargets().makeMutipleRequests(phrase, langCodes);
+            }
+        } 
     }
     
     public static void main(String[] args) {
-        new CommandLine(new Translate()).execute("-p", "Hello World!", "-lc", "es");
+        new CommandLine(new Translate()).execute(args);
     }
 }
