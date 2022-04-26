b7b45ef7fe37a830a16ffdbb713e4d7d3e30f83a
CharonTheV <bikathimartin@outlook.com>
2022-04-26 12:56:23 +0300

    Updated logic that handles all command options and errors.

--- src/main/java/npc/martin/cligoogletrans/Translate.java
+++ src/main/java/npc/martin/cligoogletrans/Translate.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 public class Translate implements Runnable {
     //option-parameter- phrase/ word to be translated
     //every command must have a phrase
-    @Option(names = { "-p" }, paramLabel = "PHRASE", description = "Phrase/ word to be translated.")
+    @Option(names = { "-p", "--phrase" }, paramLabel = "PHRASE", description = "Phrase/ word to be translated.")
     String phrase;
     
     //option-parameter- target language
@@ -24,10 +24,6 @@ public class Translate implements Runnable {
     @Option(names = { "-i", "--interractive" }, description = "Run this utility in interractive mode.")
     protected Boolean interractive = false;
     
-    //option- allow for logging of current translation
-    @Option(names = { "-l", "--log" }, description = "Allow for logging of a translation")
-    Boolean logTranslation;
-    
     //option-parameter- allow for targetting !> 5 but languages at once
     //cannot be used with -lc flag
     @Option(names = { "-lcs", "--language-codes" }, paramLabel = "MULTIPLE LANGUAGE CODES", arity = "1...5",
@@ -35,32 +31,49 @@ public class Translate implements Runnable {
     ArrayList<String> langCodes = new ArrayList(5);
     
     @Override
-    public void run() {       
-        //code to handle option errors
-        //1. When the -p flag is not invoked. It must always be used
-        if(phrase == null) {
-            System.out.println("Error! Must provide a phrase or word to translate. Use -h for Help.");    
-        //2. When the '-lc' and '-lcs' flags are used together
-        } else if(langCode != null && langCodes.isEmpty() == false) {
-            System.out.println("Fatal! -lc and -lcs cannot be used together. Use -h for Help.");
-        //if both of these tests pass, then the code can start running
-        } else {
-            //populate the hashmap on the go first before executing rest of code
-            TranslationPrerequisites.createPairs();
-
-            if(this.interractive == true) {
+    public void run() {
+        //code to handle the options logic and options errors
+        //1. The -i flag cannot be used with any other flag
+        if(this.interractive == true) {
+            if(this.phrase != null || this.langCode != null || !langCodes.isEmpty()) {
+                System.out.println("Error! The -i option cannot be used with any other option. Use -h for help.");
+            } else {
                 new InterractiveHandler().takeInput();
-            } else if(langCode != null) {
-                //do call the makeRequest() method from the class that Handles Single Requests(target)
-                new HandleSingleTarget().makeRequest(phrase, langCode);
-            } else if(langCodes.isEmpty() == false) {
-                //do call the makeMultipleRequests() method from the class that Handles Multiple Requests(targets)
-                new HandleMultiTargets().makeMutipleRequests(phrase, langCodes);
             }
-        } 
+            
+        //2. In all other cases(i.e when the -i flag is not used), the -p flag must be used.
+        //3. When the -p flag is used, either the -lc or the -lcs flag should be used, but not both at once.
+        //Remember that the -p flag calls on the class that handles single-target translation
+        } else if(this.interractive == false) {
+            if(this.phrase == null) {
+                System.out.println("Fatal! The -p flag must be used when -i is not used. Use -h for help.");
+            } else if(this.phrase != null && (this.langCode == null && langCodes.isEmpty())) {
+                System.out.println("Error! The -p flag must be used with either -lc or -lcs flag. Use -h for help.");
+            } else if(this.phrase != null && (this.langCode != null && !langCodes.isEmpty())) {
+                System.out.println("Error! The -p flag cannot be used with -lc and -lcs flags together. Use -h for help.");
+            } else if(this.phrase != null) {
+                //4. When the -lcs flag is used, it must have not more than 5 parameters and not less than 1.
+                if(!langCodes.isEmpty()) {
+                    if(langCodes.size() > 5) {
+                        System.out.println("Error. Provided too many arguments for the -lcs option. Use -h for help.");
+                    } else if(langCodes.size() < 1) {
+                        System.out.println("Error! Must provide at least one argument for the -lcs option.");
+                    } else {
+                        new HandleMultiTargets().makeMutipleRequests(phrase, langCodes);
+                    }
+                } else if(langCodes.isEmpty()) {
+                    if(this.langCode != null) {
+                        new HandleSingleTarget().makeRequest(phrase, langCode);
+                    }  
+                }
+            }
+        }    
     }
     
     public static void main(String[] args) {
-        new CommandLine(new Translate()).execute(args);
+        /*int exitCode = new CommandLine(new Translate()).execute(args);
+        System.exit(exitCode);*/
+        
+        new CommandLine(new Translate()).execute("-p", "Hello World", "-lc", "en", "-lcs", "es", "fr", "af");
     }
 }
